af40d3175cb0be80f04d3372ebb852033485da05
Imran <md.imransyam1956@gmail.com>
2023-06-10 12:22:52 +0600

    navigation

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,101 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+import { ref } from 'vue';
+
+
+import Magnify from 'vue-material-design-icons/Magnify.vue'
+import HeartOutline from 'vue-material-design-icons/HeartOutline.vue'
+import HomeOutline from 'vue-material-design-icons/HomeOutline.vue'
+import Compass from 'vue-material-design-icons/Compass.vue'
+import SendOutline from 'vue-material-design-icons/SendOutline.vue'
+import Plus from 'vue-material-design-icons/Plus.vue'
+import AccountOutline from 'vue-material-design-icons/AccountOutline.vue'
+import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue'
+import AccountPlusOutline from 'vue-material-design-icons/AccountPlusOutline.vue'
+import MenuItem from '@/Components/MenuItem.vue';
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret)
+
+ 
+let showCreatePost = ref(false);
+
+</script>
+
+<template>
+    <div id="MainLayout" class="w-full h-screen">
+        <!-- navbar -->
+        <div v-if="$page.url === '/'" id="TopNavBar" class="fixed z-30 md:hidden block w-full bg-white h-[61px] border-b border-b-gray-300">
+            <div class="flex items-center justify-between h-full">
+                <Link href="/">
+                    <img class="w-[105px] ml-6 cursor-pointer" src="/insta-logo.png">
+                </Link>
+
+                <div class="flex items-center w-[50%]">
+                    <div class="flex items-center w-full bg-gray-100 rounded-lg">
+                        <i class="fa-solid fa-magnifying-glass text-lg h-10 w-10"></i>
+                        <input
+                            type="text"
+                            placeholder="Search"
+                            class="
+                                bg-transparent
+                                w-full
+                                placeholder-[#8E8E8E]
+                                border-0
+                                ring-0
+                                focus:ring-0
+                            "
+                        >
+                    </div>
+
+                    <HeartOutline class="pl-4 pr-3" fillColor="#000000" :size="27" />
+                </div>
+            </div>
+        </div>
+
+        <div
+            v-if="$page.url !== '/'"
+            id="TopNavUser"
+            class="md:hidden fixed flex items-center justify-between z-30 w-full bg-white h-[61px] border-b border-b-gray-300"
+        >
+            <Link href="/" class="px-4">
+                <ChevronLeft :size="30" class="cursor-pointer"/>
+            </Link>
+            <div class="font-extrabold text-lg">{{ $page.props.auth.user.name }}</div>
+            <AccountPlusOutline :size="30" class="cursor-pointer px-4"/>
+        </div>
+
+        <!-- Sidebar -->
+        <div id="SideNav" class="fixed h-full bg-white xl:w-[280px] w-[80px] md:block hidden border-r border-r-gray-300">
+
+            <Link href="/">
+                <img class="xl:hidden block w-[25px] mt-10 ml-[25px] mb-10 cursor-pointer" src="/insta-logo-small.png">
+                <img class="xl:block hidden w-[120px] mt-10 ml-6 mb-10 cursor-pointer" src="/insta-logo.png">
+            </Link>
+
+            <div class="px-3">
+                <Link href="/">
+                    <MenuItem iconString="Home" class="mb-4"/>
+                </Link>
+                <MenuItem iconString="Search" class="mb-4"/>
+                <MenuItem iconString="Explore" class="mb-4"/>
+                <MenuItem iconString="Messages" class="mb-4"/>
+                <MenuItem iconString="Notifications" class="mb-4"/>
+                <MenuItem @click="showCreatePost = true" iconString="Create" class="mb-4"/>
+                <!-- <Link :href="route('users.show', { id: $page.props.auth.user.id })">
+                    <MenuItem iconString="Profile" class="mb-4"/>
+                </Link> -->
+            </div>
+        </div>
+
+
+    </div>
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import MainLayout from '@/Layouts/MainLayout.vue';
+
+</script>
+
+<template>
+    <Head title="Home" />
+    <MainLayout>
+        
+    </MainLayout>
+</template>
+
